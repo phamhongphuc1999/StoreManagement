a712002f5aac1572cfbd21a389d3018cf9d9e295
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-02-02 17:49:41 +0700

    add: flow MVVM

---
+++ StoreManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace StoreManagement.ViewModel
+{
+    public class MainViewModel: BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
